a1f91f939257fceefd492aa657dab259f7c0e238
Jarvan <450084484@qq.com>
2021-08-06 15:49:13 +0800

    fitst commit

---
+++ src/main/java/com/jarvan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jarvan.git;
+
+/**
+ * @author Jarvan
+ * @create 2021-08-06 15:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
